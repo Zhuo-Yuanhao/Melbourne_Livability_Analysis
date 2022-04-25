822d5038a4a9b41502dc5b1d6e50886edd5d3881
Yuanhao Zhuo <zhuoyz@student.unimelb.edu.au>
2022-04-25 17:35:26 +1000

    Add files via upload

---
+++ backend_b/backendMain.py
@@ -0,0 +1,8 @@
+from trail1_language import *
+from trail2_emotion import *
+from trail3_socialActivity import *
+
+if __name__ == '__main__':
+    trail1()
+    trail2()
+    trail3()
---
+++ backend_b/data/Readme.txt
@@ -0,0 +1,1 @@
+for storing temp output json file
