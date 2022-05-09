030587ef5ec7d67edcbf753773b9084cf3b82adb
FuhanSun <81127982+FuhanSun@users.noreply.github.com>
2022-05-09 15:38:00 +0800

    0509

--- Crawler/config_4.py
+++ Crawler/config_4.py
@@ -4,18 +4,17 @@ class config:
                        "consumer_secret": "H2yJAHl31zNKMX59tREerHPJMPfUQ1uSRufg5bLXIH2s8vjhby",
                        "access_token": "1083825199173197824-84MDGGgPHafO1wtaNQ1vzzlrzAZv72",
                        "access_token_secret": "CdwFcuHvCds8ucWjLE2ifTJemukjHbzKkhgKH1eiMEvdU"}},
-           {"account": {"consumer_key": "qP4NpBSNYRN9ZuPWGojd3kIi0",
-                        "consumer_secret": "0iWFIYQAFkJcDHLEaYcvZEDTwa5ro6jSqdv3Ut7b8KjqBo3UAu",
-                        "access_token": "1511621929782091778-6FKSxtDIzQgqU8iXwMBOfKEVVLBS2r",
-                        "access_token_secret": "CW2xsYNnycHbP1tMPwzqBeppym8uqkYpYrOLmf5cZlztJ"}},
-           {"account": {"consumer_key": "HtFankh70KdXLpntWlhfg4DlR",
+              {"account": {"consumer_key": "HtFankh70KdXLpntWlhfg4DlR",
                         "consumer_secret": "R69i8hkqSdxjqvi0dRT4etizgzxC5CjVsrfvBmuBcPWIjugaJx",
                         "access_token": "1515968815108333570-BkdyfgxnLjJBXxrJBJTrb9qB7OgIGQ",
                         "access_token_secret": "rw03SkUAj1f2rd0jQUfx8XZtvRwOMOdZH12LLpTI6Iara"}},
-           {"account": {"consumer_key": "SclGBcZ9SwFqADhW9leo0O2gn",
+              {"account": {"consumer_key": "qP4NpBSNYRN9ZuPWGojd3kIi0",
+                        "consumer_secret": "0iWFIYQAFkJcDHLEaYcvZEDTwa5ro6jSqdv3Ut7b8KjqBo3UAu",
+                        "access_token": "1511621929782091778-6FKSxtDIzQgqU8iXwMBOfKEVVLBS2r",
+                        "access_token_secret": "CW2xsYNnycHbP1tMPwzqBeppym8uqkYpYrOLmf5cZlztJ"}},
+              {"account": {"consumer_key": "SclGBcZ9SwFqADhW9leo0O2gn",
                         "consumer_secret": "N9RlU6efOww4MUYvgRQMb4YbxBEIS0JiW4MjhUaZrDKNZNnbpU",
                         "access_token": "1516735109348851715-D3MICPn1zSy73TsrHk22F72cNQxbgm",
                         "access_token_secret": "gwlBkdM6pMUXRxk63s1Dt9Ztwhv8Pq5Nehn6yJ4iyqx3c"}}
-
-           ]
+              ]
 
